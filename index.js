e59f36cdee41ea745fcd7ec5598b90a831f3cf23
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-04-02 19:35:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+const driver = {
+  name: 'Sam',
+  address: '11 Broadway'
+};
+
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {
+    name: 'Sam',
+    address: '11 Broadway'
+  };
+  newDriver[key] = value;
+  return newDriver;
+};
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+};
+
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {
+    name: 'Sam',
+    address: '11 Broadway'
+  };
+  delete newDriver[key];
+  return newDriver;
+};
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
